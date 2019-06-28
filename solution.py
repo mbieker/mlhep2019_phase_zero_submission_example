7cda4d9c06e7024676148c4cef4fc46a47d77ef5
Martin Bieker <martin.bieker@cern.ch>
2019-06-28 17:13:01 +0200

    Fix swap error, maybe?

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def main():
     predicted_result = []
     df = np.loadtxt(input_dir + '/data.data')
     df = pd.DataFrame(df, columns=['column 1', 'column 2'])
-    df['result'] = 10e10
+    df['result'] = 424242
     np.savetxt(output_dir + '/data.predict', np.array(df['result']))
     return 0
 
